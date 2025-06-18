5d50ba692c7af73c2f1f1d48a8c0258779eba3e4
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-06-18 00:32:21 -0300

    ✨ feat: define função para criar conexão com db

---
+++ app/database.py
@@ -0,0 +1,21 @@
+import psycopg
+import os
+from dotenv import load_dotenv
+from psycopg import Error
+
+load_dotenv()
+
+def cria_conexao_db():
+    """
+    Cria e retorna uma nova conexão com o banco de dados PostgreSQL.
+    Lê a URL de conexão da variável de ambiente DATABASE_URL.
+    """
+    try:
+        database_url = os.getenv("DATABASE_URL")
+
+        conn = psycopg.connect(database_url)
+        print("Conexão com o PostgreSQL estabelecida com sucesso!")
+        return conn
+
+    except Error as error:
+        print(f"Erro ao conectar ao PostgreSQL: {error}")
