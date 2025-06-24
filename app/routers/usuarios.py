85916f0737ab25c82cf37139fe499c360fb02e17
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2025-06-24 20:22:53 -0300

    ✨ feat: adiciona rota para pegar discente em específico

--- app/routers/usuarios.py
+++ app/routers/usuarios.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 
-from app.schemas.usuario_schema import DiscenteCreate, DiscenteRead
+from app.schemas.usuario_schema import DiscenteCreate, DiscenteRead, DiscenteUpdate
 
 from app.repositories import discente_repository
 
@@ -22,7 +22,20 @@ def create_discente(discente_data: DiscenteCreate):
             detail=f"Erro ao criar discente: {e}"
         )
 
-@router.get("usuarios/discente", response_model=List[DiscenteRead])
+@router.get("/usuarios/discente", response_model=List[DiscenteRead])
 def get_all_discentes():
     """Endpoint para listar todos os discentes cadastrados."""
     return discente_repository.get_all_discentes()
+
+@router.get("/usuarios/discente/{cpf}", response_model=DiscenteRead, status_code=status.HTTP_201_CREATED)
+def get_discente_by_cpf(cpf: str):
+    """Endpoint para listar um discente específico do banco de dados."""
+    try:
+        discente_selecionado = discente_repository.get_discente_by_cpf(cpf)
+        return discente_selecionado
+        
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Erro ao criar discente: {e}"
+        )
